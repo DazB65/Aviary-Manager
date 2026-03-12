5cd050fbee49429e45de12f3ab5fcd3af2cc3527
DazB <darren.berich@me.com>
2026-03-13 09:49:27 +1000

    fix: make enum migrations idempotent to prevent 'already exists' errors on redeploy

--- drizzle/0003_first_mindworm.sql
+++ drizzle/0003_first_mindworm.sql
@@ -1,5 +1,5 @@
-ALTER TYPE "public"."bird_status" ADD VALUE 'fledged' BEFORE 'deceased';--> statement-breakpoint
-ALTER TYPE "public"."event_type" ADD VALUE 'supplements' BEFORE 'other';--> statement-breakpoint
+DO $$ BEGIN ALTER TYPE "public"."bird_status" ADD VALUE 'fledged' BEFORE 'deceased'; EXCEPTION WHEN duplicate_object THEN NULL; END $$;--> statement-breakpoint
+DO $$ BEGIN ALTER TYPE "public"."event_type" ADD VALUE 'supplements' BEFORE 'other'; EXCEPTION WHEN duplicate_object THEN NULL; END $$;--> statement-breakpoint
 ALTER TABLE "events" ADD COLUMN "allBirds" boolean DEFAULT false;--> statement-breakpoint
 ALTER TABLE "events" ADD COLUMN "seriesId" varchar(64);--> statement-breakpoint
 ALTER TABLE "events" ADD COLUMN "recurrenceUnit" varchar(16);--> statement-breakpoint
--- drizzle/0004_marvelous_phantom_reporter.sql
+++ drizzle/0004_marvelous_phantom_reporter.sql
@@ -1,2 +1,2 @@
-ALTER TYPE "public"."egg_outcome" ADD VALUE 'fledged';--> statement-breakpoint
+DO $$ BEGIN ALTER TYPE "public"."egg_outcome" ADD VALUE 'fledged'; EXCEPTION WHEN duplicate_object THEN NULL; END $$;--> statement-breakpoint
 ALTER TABLE "clutchEggs" ADD COLUMN "outcomeDate" date;
