646a4e01e3016c6904cb215280d90a640d3110e5
DazB <darren.berich@me.com>
2026-03-12 14:10:35 +1000

    Landing: remove opacity-40 from decorative bird logos so they match auth page

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -138,8 +138,8 @@ export default function Landing() {
         </div>
 
         {/* Decorative birds */}
-        <img src="/logo.svg" alt="" className="absolute top-10 left-10 w-28 h-28 opacity-40 rotate-[-15deg]" />
-        <img src="/logo.svg" alt="" className="absolute bottom-10 right-10 w-24 h-24 opacity-40 rotate-[10deg]" />
+        <img src="/logo.svg" alt="" className="absolute top-10 left-10 w-28 h-28 rotate-[-15deg]" />
+        <img src="/logo.svg" alt="" className="absolute bottom-10 right-10 w-24 h-24 rotate-[10deg]" />
       </section>
 
       {/* Features */}
